ba12f4b3a052aa2562ef52f93bf7e19b81d0117d
Mohammad Razaul Islam <shovon231@gmail.com>
2023-05-01 07:01:35 +0000

    initial seeds for users table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,23 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, email)
-VALUES ('John Doe', 'johndoe@example.com'), ('Jane Smith', 'janesmith@example.com'), ('Bob Johnson', 'bobjohnson@example.com');
+-- INSERT INTO users (name, email)
+-- VALUES ('John Doe', 'johndoe@example.com'), ('Jane Smith', 'janesmith@example.com'), ('Bob Johnson', 'bobjohnson@example.com');
+INSERT INTO users (name, email) VALUES ('John Smith', 'john.smith@example.com');
+INSERT INTO users (name, email) VALUES ('Emily Johnson', 'emily.johnson@example.com');
+INSERT INTO users (name, email) VALUES ('Michael Brown', 'michael.brown@example.com');
+INSERT INTO users (name, email) VALUES ('Sarah Davis', 'sarah.davis@example.com');
+INSERT INTO users (name, email) VALUES ('David Wilson', 'david.wilson@example.com');
+INSERT INTO users (name, email) VALUES ('Jessica Lee', 'jessica.lee@example.com');
+INSERT INTO users (name, email) VALUES ('Ryan Mitchell', 'ryan.mitchell@example.com');
+INSERT INTO users (name, email) VALUES ('Avery Garcia', 'avery.garcia@example.com');
+INSERT INTO users (name, email) VALUES ('Oliver Robinson', 'oliver.robinson@example.com');
+INSERT INTO users (name, email) VALUES ('Grace Hernandez', 'grace.hernandez@example.com');
+INSERT INTO users (name, email) VALUES ('Ethan Phillips', 'ethan.phillips@example.com');
+INSERT INTO users (name, email) VALUES ('Emma Carter', 'emma.carter@example.com');
+INSERT INTO users (name, email) VALUES ('Lucas Evans', 'lucas.evans@example.com');
+INSERT INTO users (name, email) VALUES ('Avery Green', 'avery.green@example.com');
+INSERT INTO users (name, email) VALUES ('Mia Martinez', 'mia.martinez@example.com');
+INSERT INTO users (name, email) VALUES ('Jackson Anderson', 'jackson.anderson@example.com');
+INSERT INTO users (name, email) VALUES ('Ella Flores', 'ella.flores@example.com');
+INSERT INTO users (name, email) VALUES ('William Ramirez', 'william.ramirez@example.com');
+INSERT INTO users (name, email) VALUES ('Nora Carter', 'nora.carter@example.com');
+INSERT INTO users (name, email) VALUES ('Henry Johnson', 'henry.johnson@example.com');
